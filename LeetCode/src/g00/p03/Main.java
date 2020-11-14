9a916d426cc3ca8eb0c4006565c0e1f56f6777a5
jamalchen <jamalchen0981@gmail.com>
2020-11-14 16:08:11 +0800

    3. Longest Substring Without Repeating Characters

---
+++ LeetCode/src/g00/p03/Main.java
@@ -0,0 +1,50 @@
+/**
+ * 
+ */
+package g00.p03;
+
+/**
+ * 3. Longest Substring Without Repeating Characters
+ * Medium
+ * 
+ * @author jamal
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println(new Solution().lengthOfLongestSubstring("abcabcbb"));
+		System.out.println(new Solution().lengthOfLongestSubstring("bbbbb"));
+		System.out.println(new Solution().lengthOfLongestSubstring("pwwkew"));
+		System.out.println(new Solution().lengthOfLongestSubstring(""));
+	}
+
+}
+
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int[] x = new int[256];
+        for(int i = 0; i < x.length; ++i) {
+        	x[i] = -1;
+        }
+        int max = 0;
+        int pivot = 0;
+        for(int i = 0; i < s.length(); ++i) {
+        	char c = s.charAt(i);
+        	if(x[c] >= 0 && x[c] >= pivot) {
+        		if(max < i - pivot) {
+        			max = i - pivot;
+        		}
+        		pivot = x[c] + 1;
+        	}
+        	x[c] = i;
+        }
+        if(max < s.length() - pivot) {
+        	max = s.length() - pivot;
+        }
+        return max;
+    }
+}
