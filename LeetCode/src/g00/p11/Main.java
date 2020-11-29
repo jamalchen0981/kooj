fda4d0434d974986e98b0452f8a31c53762d5e7e
jamalchen <jamalchen0981@gmail.com>
2020-11-29 13:00:49 +0800

    11. Container With Most Water

---
+++ LeetCode/src/g00/p11/Main.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package g00.p11;
+
+/**
+ * 11. Container With Most Water
+ * Medium
+ * 2ms, 40.2MB
+ * 
+ * @author jamal
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println(new Solution().maxArea(new int[] {1, 1}));
+		System.out.println(new Solution().maxArea(new int[] {1,8,6,2,5,4,8,3,7}));
+		System.out.println(new Solution().maxArea(new int[] {4,3,2,1,4}));
+		System.out.println(new Solution().maxArea(new int[] {1,2,1}));
+		System.out.println(new Solution().maxArea(new int[] {1,2}));
+		System.out.println(new Solution().maxArea(new int[] {1,0,0,0,0,0,0,2,2}));
+		System.out.println(new Solution().maxArea(new int[] {1,2,3,4,5,6,7,8,9,10}));
+	}
+
+}
+
+class Solution {
+    public int maxArea(int[] height) {
+        int maxarea = 0, l = 0, r = height.length - 1;
+        while (l < r) {
+            maxarea = Math.max(maxarea, Math.min(height[l], height[r]) * (r - l));
+            if (height[l] < height[r])
+                l++;
+            else
+                r--;
+        }
+        return maxarea;
+    }
+}
+
