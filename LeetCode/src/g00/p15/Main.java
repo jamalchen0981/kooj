cb0e573db92b235585beb2e3fcad8ab1c307fc48
jamalchen <jamalchen0981@gmail.com>
2021-04-06 17:09:42 +0800

    15. 3Sum

--- LeetCode/src/g00/p15/Main.java
+++ LeetCode/src/g00/p15/Main.java
@@ -12,7 +12,7 @@ import java.util.Map;
 /**
  * 15. 3Sum
  * Medium
- * 69 ms, 44.5 MB
+ * 41 ms, 45.1 MB
  * 
  * @author jamal
  *
@@ -38,38 +38,35 @@ class Solution {
         if(nums.length > 0) {
 //        	0 <= nums.length <= 3000
 //        	-105 <= nums[i] <= 105
-        	final int size = 200010;
-        	int base = 100000;
-        	int[] array = new int[size];
-        	for(int i = 0; i < array.length; ++i) {
-        		array[i] = 0;
-        	}
+        	Map<Integer, Integer> nMap = new HashMap<Integer, Integer>();
+        	Map<Integer, Integer> pMap = new HashMap<Integer, Integer>();
         	List<Integer> nlist = new ArrayList<>();
         	List<Integer> plist = new ArrayList<>();
         	List<Integer> zlist = new ArrayList<>();
         	Arrays.sort(nums);
         	for(int i = 0; i < nums.length; ++i) {
-        		array[ base + nums[i] ] = 1;
         		if(nums[i] < 0) {
         			nlist.add(0, nums[i]);
+        			nMap.put(nums[i], 1);
         		} else if(nums[i] == 0) {
         			zlist.add(0);
         		} else {
         			plist.add(nums[i]);
+        			pMap.put(nums[i], 1);
         		}
         	}
         	Map<Long, Integer> map = new HashMap<Long, Integer>();
         	// 2 negative, 1 positive
         	for(int i = 0; i < nlist.size(); ++i) {
         		for(int j = i + 1; j < nlist.size(); ++j) {
-        			int x = -(nlist.get(i) + nlist.get(j)) + base;
-        			if(x >= size) {
+        			int x = -(nlist.get(i) + nlist.get(j));
+        			if(x >= 100000) {
         				break;
         			}
-        			if(array[x] > 0) {
+        			if(pMap.get(x) != null) {
         				Long m = nlist.get(i) * 100000L +  nlist.get(j);
         				if(map.get(m) == null) {
-        					list2D.add(Arrays.asList(new Integer[] {nlist.get(i), nlist.get(j), x - base}));
+        					list2D.add(Arrays.asList(new Integer[] {nlist.get(i), nlist.get(j), x}));
         					map.put(m, 1);
         				}
         			}
@@ -78,14 +75,14 @@ class Solution {
         	// 2 positive, 1 negative
         	for(int i = 0; i < plist.size(); ++i) {
         		for(int j = i + 1; j < plist.size(); ++j) {
-        			int x = -(plist.get(i) + plist.get(j)) + base;
-        			if(x < 0) {
+        			int x = -(plist.get(i) + plist.get(j));
+        			if(x < -100000) {
         				break;
         			}
-        			if(array[x] > 0) {
+        			if(nMap.get(x) != null) {
         				Long m = plist.get(i) * 100000L + plist.get(j);
         				if(map.get(m) == null) {
-        					list2D.add(Arrays.asList(new Integer[] {x - base, plist.get(i), plist.get(j)}));
+        					list2D.add(Arrays.asList(new Integer[] {x, plist.get(i), plist.get(j)}));
         					map.put(m, 1);
         				}
         			}
@@ -94,11 +91,11 @@ class Solution {
         	// 1 zero, 1 positive, 1 negative, if necessary
         	if(zlist.size() > 0) {
         		for(int i = 0; i < nlist.size(); ++i) {
-        			int x = -nlist.get(i) + base;
-        			if(array[x] > 0) {
+        			int x = -nlist.get(i);
+        			if(pMap.get(x) != null) {
         				Long m = (long)nlist.get(i);
         				if(map.get(m) == null) {
-        					list2D.add(Arrays.asList(new Integer[] {nlist.get(i), 0, x - base}));
+        					list2D.add(Arrays.asList(new Integer[] {nlist.get(i), 0, x}));
         					map.put(m, 1);
         				}
         			}
